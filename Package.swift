c04e268381ce763e5f95f6fdea0b148d9bb4d22a
Dzmitry Smaliakou <dm.smolyakov@gmail.com>
2024-06-01 00:22:59 +0300

    Add files via upload
    test

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "DocReader",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "Full", targets: ["FullTarget"]),
+        .library(name: "Barcode", targets: ["BarcodeTarget"]),
+    ],
+    targets: [
+        .target(
+            name: "FullTarget",
+            dependencies: [
+                .package(url: "https://github.com/Darkzwer/FullCore", branch: "main")
+            ],
+        ),
+        .target(
+            name: "BarcodeTarget",
+            dependencies: [
+                .package(url: "https://github.com/Darkzwer/BarcodeCore", branch: "main")
+            ],
+        )
+    ]
+)
